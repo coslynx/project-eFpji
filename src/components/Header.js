fe3cb01fe2cd80dae980ddf071bb347179d4f565
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-28 07:36:33 +0530

    Added: `src/components/Header.js`

---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+// Filename: Header.js
+// Language: JavaScript
+
+import React from 'react';
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/portfolio">
+              <a>Portfolio</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <a>Contact</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
