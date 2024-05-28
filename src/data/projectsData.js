b3677577af30bbea75b70ba89c440306c5f2015f
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-28 07:37:05 +0530

    Added: `src/data/projectsData.js`

---
+++ src/data/projectsData.js
@@ -0,0 +1,30 @@
+// projectsData.js
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Project 1",
+    image: "project1.jpg",
+    description: "Description for Project 1",
+    demoLink: "https://demo-link.com/project1",
+    githubLink: "https://github.com/project1"
+  },
+  {
+    id: 2,
+    title: "Project 2",
+    image: "project2.jpg",
+    description: "Description for Project 2",
+    demoLink: "https://demo-link.com/project2",
+    githubLink: "https://github.com/project2"
+  },
+  {
+    id: 3,
+    title: "Project 3",
+    image: "project3.jpg",
+    description: "Description for Project 3",
+    demoLink: "https://demo-link.com/project3",
+    githubLink: "https://github.com/project3"
+  }
+];
+
+export default projectsData;
